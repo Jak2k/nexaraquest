c29dffca519d5d01710f43298638567a3db7f77a
Jak2k <76660118+Jak2k@users.noreply.github.com>
2023-12-29 17:54:07 +0000

    style: :art: format code

--- nexara_text_engine/src/scenify.rs
+++ nexara_text_engine/src/scenify.rs
@@ -1,9 +1,11 @@
 #[macro_export]
 macro_rules! scenify {
     ($scene_type:ty, $context_type:ty, $initial_scene:expr, $body:expr) => {
-
         impl Scenes<$scene_type, $context_type> for $scene_type {
-            fn get_current_scene(self: &$scene_type, context: &mut $context_type) -> Scene<$scene_type> {
+            fn get_current_scene(
+                self: &$scene_type,
+                context: &mut $context_type,
+            ) -> Scene<$scene_type> {
                 $body(self, context)
             }
 
